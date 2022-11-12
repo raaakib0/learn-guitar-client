d46b79cb4d5f51ce735ac03746be0614acc18a64
md-rakibul-islam <raaakib0@gmail.com>
2022-11-12 21:06:39 +0600

    firebase configuration

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDGm-QWezIrcAJeN8U2CF9lJWt9dZLLfFQ",
+    authDomain: "learn-guitar-53243.firebaseapp.com",
+    projectId: "learn-guitar-53243",
+    storageBucket: "learn-guitar-53243.appspot.com",
+    messagingSenderId: "211442211407",
+    appId: "1:211442211407:web:551373447875f70003a83a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
