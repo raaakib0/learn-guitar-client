3dbc51968b534ccce12ff6eeed7d1d3e5c69229c
md-rakibul-islam <raaakib0@gmail.com>
2022-11-17 00:31:34 +0600

    course details add

--- src/layout/Pages/Course/Course/Course.js
+++ src/layout/Pages/Course/Course/Course.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const Course = () => {
+    const course = useLoaderData();
+    const { title, details, image_url,category_id } = course;
     return (
-        <div>
-            <h2>Course</h2>
-        </div>
+        <Card >
+            <Card.Img variant="top" src={image_url} />
+            <Card.Body>
+                <Card.Title>{ title}</Card.Title>
+                <Card.Text>
+                   {details}
+                </Card.Text>
+                <Link to={`/category/${category_id}`} >
+                    <Button variant="primary">All Tutorial in This Course</Button>
+                </Link>
+            </Card.Body>
+        </Card>
     );
 };
 
